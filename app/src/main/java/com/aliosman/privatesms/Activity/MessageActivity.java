899ad05597c313e40aee929950efbcc3462ff577
Ali Osman OKTAR <aliosmanoktar@gmail.com>
2019-08-29 00:28:58 +0300

    Add Message Screen Views

--- app/src/main/java/com/aliosman/privatesms/Activity/MessageActivity.java
+++ app/src/main/java/com/aliosman/privatesms/Activity/MessageActivity.java
@@ -1,16 +1,20 @@
+/*
+ * Copyright (c) 2019. Ali Osman OKTAR
+ * aliosmanoktar@gmail.com
+ */
+
 package com.aliosman.privatesms.Activity;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
 import com.aliosman.privatesms.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(this,ConvarsationActivity.class));
+        setContentView(R.layout.activity_message);
     }
 }
