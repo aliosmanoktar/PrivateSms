e4fe07fedb8b1b935c9ff16b50dc3e7562c676e9
Ali Osman OKTAR <aliosmanoktar@gmail.com>
2019-09-10 13:33:24 +0300

    Added Notification

--- app/src/main/java/com/aliosman/privatesms/Receiver/SmsReceiver.java
+++ app/src/main/java/com/aliosman/privatesms/Receiver/SmsReceiver.java
@@ -6,14 +6,22 @@
 package com.aliosman.privatesms.Receiver;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsMessage;
 import com.aliosman.privatesms.SmsManager.MySmsManager;
 
+import java.util.Random;
+
 public class SmsReceiver extends BroadcastReceiver {
     private String TAG = getClass().getName();
+    private String ChannelID="10561";
+    private String ChannelName="Messages";
     @Override
     public void onReceive(Context context, Intent intent) {
         Object[] smsExtra = (Object[]) intent.getExtras().get("pdus");
@@ -36,5 +44,22 @@ public class SmsReceiver extends BroadcastReceiver {
         Log.e(TAG, "onReceive: " );
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
         notificationManagerCompat.notify(1, notification);*/
+        ShowNotification(context,body,phoneNumber);
     }
+    private void ShowNotification(Context ctx,String body,String address){
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(ctx,ChannelID)
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setContentTitle(address)
+                .setContentText(body)
+                .setAutoCancel(true);
+        NotificationManager notificationManager =
+                (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationBuilder.setChannelId(ChannelID);
+            NotificationChannel mChannel = new NotificationChannel(ChannelID, ChannelName,NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+    }
+
 }
