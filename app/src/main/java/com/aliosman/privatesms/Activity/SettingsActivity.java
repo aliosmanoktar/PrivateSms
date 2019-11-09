1f514369d0afe2caaa3ac1baaaf5010745bb2eec
Ali Osman OKTAR <aliosmanoktar@gmail.com>
2019-11-09 15:10:27 +0300

    Added Settings Activity(Empty)

---
+++ app/src/main/java/com/aliosman/privatesms/Activity/SettingsActivity.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019. Ali Osman OKTAR
+ * aliosmanoktar@gmail.com
+ */
+
+package com.aliosman.privatesms.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aliosman.privatesms.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
